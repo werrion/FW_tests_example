d4c0e0ebc7e1f05748ff773bf65647ba06999b4c
oleg.krivov <oleg.krivov@ab-games.com>
2021-07-13 17:28:40 +0300

    Add new files

---
+++ Tests/api_tests.py
@@ -0,0 +1,16 @@
+import allure
+
+@allure.story("Тесты для проверки Ассертейшенов")
+@allure.step("Тест Ok статус")
+def test_ok():
+    assert 1 == 1
+
+@allure.story("Тесты для проверки Ассертейшенов")
+@allure.step("Тест fail статус")
+def test_fail():
+    assert 1 == 2
+
+@allure.story("Тесты для проверки Ассертейшенов")
+@allure.step("Тест Ok1 статус")
+def test_ok1():
+    assert 2 == 2
