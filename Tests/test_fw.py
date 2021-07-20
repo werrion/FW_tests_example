45143a3f3a86f1f55831ccb978db419367cf5e18
oleg.krivov <oleg.krivov@ab-games.com>
2021-07-20 16:56:14 +0300

    Add new ver Util + data files sections

--- Tests/test_fw.py
+++ Tests/test_fw.py
@@ -1,7 +1,12 @@
 import subprocess
 
-def test_set_variable_type_int_test():
+def test_set_variable_type_int():
     result = subprocess.run([r"C:\Users\oleg.krivov\Desktop\FW_tests_example\Util\ABUtility.exe",
-                             "1","1","0","set","system.userprofile.money","222"],
+                             "0","1","0","set","system.userprofile","money","222"],
                              stdout=subprocess.PIPE, encoding='utf-8')
     print("Тест 1 - Set Var_test", result.stdout)
+
+
+
+
+test_set_variable_type_int()
